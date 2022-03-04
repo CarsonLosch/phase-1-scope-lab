723587159f2aca67f5be83a4954ae8c2adde550c
CarsonLosch <95552371+CarsonLosch@users.noreply.github.com>
2022-03-03 18:41:40 -0800

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+var customerName = "bob";
+const leastFavoriteCustomer = "Rex"
+
+upperCaseCustomerName = function() {
+    customerName = customerName.toUpperCase();
+    return 
+}
+
+
+function setBestCustomer() {
+    bestCustomer = "not bob"
+    return bestCustomer;
+}
+
+overwriteBestCustomer = function() {
+    bestCustomer = "maybe bob"
+    return bestCustomer;
+}
+
+changeLeastFavoriteCustomer = function() {
+    leastFavoriteCustomer = "bob"
+    return leastFavoriteCustomer;
+}
